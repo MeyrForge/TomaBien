046efc3a532ab3551c180f8c51ae9783c7a2d637
Aguino Aylen Maria <ailenaguino@gmail.com>
2025-09-19 16:40:00 -0300

    new notification icon

--- app/src/main/java/com/meyrforge/tomabien/my_medications/domain/usecases/AddAlarmUseCase.kt
+++ app/src/main/java/com/meyrforge/tomabien/my_medications/domain/usecases/AddAlarmUseCase.kt
@@ -7,8 +7,9 @@ import javax.inject.Inject
 
 class AddAlarmUseCase @Inject constructor(private val repository: MedicationRepository) {
     suspend operator fun invoke(requestCode: Int, hour:Int, minute:Int, medicationId: Int):Boolean {
-        return when (repository.addAlarm(Alarm(requestCode, hour, minute, medicationId))) {
+        return when (val result = repository.addAlarm(Alarm(requestCode, hour, minute, medicationId))) {
             is Result.Success -> {
+                result.data
                 true
             }
 
